3d4c99832d7d79b4536613e0d8480af65fe247e4
João Pedro <pedrolockeson@gmail.com>
2020-06-05 18:16:34 -0300

    Completamente atualizado e funcionando para a plataforma GIT | Atualizada a mecânica de interação com o usuario entre as notas e o "jogador" | Testes falhos de benchmark nao resolvido

--- scripts/Tela.py
+++ scripts/Tela.py
@@ -1,24 +1,31 @@
 import pygame
-from scripts import Notas, Player
+from scripts import Player, Notas
 
 pygame.init()
 
 
 class Tela:
+
     def __init__(self):
         self.janela = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
         self.notas = []
         self.rodando = True
         self.ctrl = False
         self.kw = False
-
+        self.fps = 0
+        self.antes = 0
         self.player = Player.Player()
 
     def render(self, janela, objTela):
+        self.fps = self.fps + 1
+        if pygame.time.get_ticks() - self.antes >= 1000:
+            self.antes = pygame.time.get_ticks()
+            print(self.fps)
+            self.fps = 0
         self.janela.fill((255, 255, 255))
         for nota in self.notas:
             nota.render(janela)
-            nota.tick(objTela)
+            nota.tick(objTela, self.player)
 
         self.player.render(janela)
 
@@ -38,6 +45,12 @@ class Tela:
                 # INTERAÇÕES DO USUÁRIO
                 if eventos.key == pygame.K_a:
                     self.player.btn1 = 0
+                if eventos.key == pygame.K_s:
+                    self.player.btn2 = 0
+                if eventos.key == pygame.K_j:
+                    self.player.btn3 = 0
+                if eventos.key == pygame.K_k:
+                    self.player.btn4 = 0
             elif eventos.type == pygame.KEYUP:
                 if eventos.key == pygame.K_w:
                     self.kw = False
@@ -46,4 +59,9 @@ class Tela:
                 # INTERAÇÕES DO USUÁRIO
                 if eventos.key == pygame.K_a:
                     self.player.btn1 = 1
-    #TESTE 1 12 3
+                if eventos.key == pygame.K_s:
+                    self.player.btn2 = 1
+                if eventos.key == pygame.K_j:
+                    self.player.btn3 = 1
+                if eventos.key == pygame.K_k:
+                    self.player.btn4 = 1
