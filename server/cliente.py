07b75fd17bc52ac05b7a79ccfed4dcb4931d6a24
Emanuel Alves <emanuelalvesps5@gmail.com>
2020-06-12 17:40:23 -0300

    Comentado os arquivos do multiplayer local

--- server/cliente.py
+++ server/cliente.py
@@ -2,13 +2,17 @@ import socket
 import pickle
 from server import Player
 
-ip = "localhost"
+ip = "localhost" # ip local
 porta = 50001
 
-obj = Player.Player()
+obj = Player.Player() # Simula o que seria um objeto real do jogo
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((ip, porta))
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Define o tipo de conexão.
+                                                            # 1 parametro: define a conexão com um ipv4
+                                                            # 2 parametro: define o tipo de conexão, tcp/ip
+cliente.connect((ip, porta)) # Tenta conexão com o servidor
 
-print(cliente.recv(1024).decode())
-cliente.send(pickle.dumps(obj))
+print(cliente.recv(1024).decode())  # Recebe o que o servidor mandou para o cliente e decodifica
+                                    # Os dados são enviados e recebidos em bytes
+                                    # O parâmetro de "recv()" [e o tamanho máximo do byte que vai ser recebido
+cliente.send(pickle.dumps(obj)) # Envia o objeto player codificado em bytes para o servidor
---
+++ server/servidor.py
@@ -0,0 +1,29 @@
+import socket
+import pickle
+
+
+ip = "localhost"
+porta = 50001
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Define o tipo de conexão.
+                                                            # 1 parametro: define a conexão com um ipv4
+                                                            # 2 parametro: define o tipo de conexão, tcp/ip
+
+server.bind((ip, porta)) # Define o ip e a porta do server
+
+server.listen(2) # Define o numero máximo de conexões simultâneas
+
+print(f"Ouvindo em {ip}:{porta}")
+
+while True:
+    cliente, id = server.accept()   # Aceita a conexão
+                                    # cliente: Vai ser a conexão estabelecida, usando esse objeto
+                                    # da pra ver as informações recebidas e da pra enviar informações
+                                    # id, é um número que é atribuido quando aceita a comunicação, com ele da pra
+                                    # diferenciar conexões diferentes
+    print(f"Conectado ao {id}")
+    cliente.send(b"Conexao estabelecida") # Envia uma string convertida para bytes para o cliente
+    obj = pickle.loads(cliente.recv(1024)) # Decodifica a mensagem enviada pelo cliente, o objeto
+    print(obj.msg)
+    obj.tick()
+    cliente.close() # Encerra a conexão
